fb628fd34d8ef2b96a04655d49ebd897d35f6ebe
Darksied95 <aliyuefe9501@gmail.com>
2023-03-09 20:56:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// https://api.adviceslip.com/advice
+
+
+fetch("https://api.adviceslip.com/advice")
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+//LAZY- Akorede, Ariyo, Charles, Fortune, Glory, Chima, Yetunde
+//HARDWORKERS Goodness, Kosi, Oluwatoyin, Edwin, Sam, 
