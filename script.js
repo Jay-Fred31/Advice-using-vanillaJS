935cb171aa14728f178686e6a0494f1c47c56643
Darksied95 <aliyuefe9501@gmail.com>
2023-03-16 20:05:46 +0100

    refactored code

--- script.js
+++ script.js
@@ -2,31 +2,22 @@ let adviceNumber = document.getElementById("advice-number")
 let quote = document.getElementById("quote")
 let dice = document.getElementById("dice-container")
 
+const url = "https://api.adviceslip.com/advice"
 
-fetch("https://api.adviceslip.com/advice")
-    .then(res => res.json())
-    .then(data => {
-        adviceNumber.textContent = data.slip.id
-        quote.textContent = data.slip.advice
-    })
-    .catch(err => console.log("something went wrong"))
+fetchAdvice(url)
 
 
 dice.addEventListener("click", () => {
-    fetch("https://api.adviceslip.com/advice")
+    fetchAdvice(url)
+})
+
+
+function fetchAdvice(url) {
+    fetch(url)
         .then(res => res.json())
         .then(data => {
             adviceNumber.textContent = data.slip.id
             quote.textContent = data.slip.advice
         })
-        .catch(err => console.log("something went wrong"))
-    // try {
-    //     let response = await fetch("https://apii.adviceslip.com/advice")
-    //     let data = await response.json()
-    //     adviceNumber.textContent = data.slip.id
-    //     quote.textContent = data.slip.advice
-    // } catch (error) {
-    //     console.log("Something went wrong");
-    // }
-
-})
+        .catch(err => console.log("Something Went Wrong"))
+}
