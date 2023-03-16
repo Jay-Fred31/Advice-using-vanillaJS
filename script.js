ab7fecec0fd63e91ec7bb992e8a949efc9c59e7f
Darksied95 <aliyuefe9501@gmail.com>
2023-03-16 19:54:30 +0100

    added fetch functionality on dice click

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let adviceNumber = document.getElementById("advice-number")
+let quote = document.getElementById("quote")
+let dice = document.getElementById("dice-container")
+
 
 fetch("https://api.adviceslip.com/advice")
     .then(res => res.json())
     .then(data => {
-
-        console.log(data);
-        let id = data.slip.id
-        adviceNumber.textContent = id
+        adviceNumber.textContent = data.slip.id
+        quote.textContent = data.slip.advice
     })
-    .catch(err => console.log("Something went wrong"))
+    .catch(err => console.log("something went wrong"))
 
 
+dice.addEventListener("click", () => {
+    fetch("https://api.adviceslip.com/advice")
+        .then(res => res.json())
+        .then(data => {
+            adviceNumber.textContent = data.slip.id
+            quote.textContent = data.slip.advice
+        })
+        .catch(err => console.log("something went wrong"))
+    // try {
+    //     let response = await fetch("https://apii.adviceslip.com/advice")
+    //     let data = await response.json()
+    //     adviceNumber.textContent = data.slip.id
+    //     quote.textContent = data.slip.advice
+    // } catch (error) {
+    //     console.log("Something went wrong");
+    // }
+
+})
